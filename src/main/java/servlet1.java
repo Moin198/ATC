4db47a0c88a1f54d1855d79b4060fcf7e6726a2e
Santosh <santosh@atjoin.in>
2022-09-10 13:09:48 +0530

    initial commit

---
+++ src/main/java/servlet1.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet1
+ */
+//@WebServlet("/servlet1")
+public class servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public servlet1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		response.setContentType("text/html");
+		
+	PrintWriter out=response.getWriter();
+	
+	String name=request.getParameter("userid");
+	out.print("<html>");
+	out.print("<body>");
+	out.print("<h1>");
+	out.print("Welcome:" +name);
+	out.print("</body>");
+	out.print("</html>");
+
+	}
+
+}
